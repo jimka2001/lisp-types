fa03f649715117956fe5b84316d38db1333f4649
Jim Newton <jnewton@lrde.epita.fr>
2017-12-18 17:02:47 +0100

    removed unused class bdd-typecase-node

--- lisp-types/bdd-typecase.lisp
+++ lisp-types/bdd-typecase.lisp
@@ -21,106 +21,10 @@
 
 (in-package :lisp-types)
 
-(defclass bdd-typecase-node (bdd-leaf)
-  ((satisfies :initarg :satisfies)
-   (proxy :accessor bdd-proxy)))
-
-(defmethod slot-unbound (class (obj bdd-typecase-node) (slot-name (eql 'proxy)))
-  (setf (slot-value obj 'proxy) (%bdd-node (bdd-serialize obj) *bdd-true* *bdd-false*)))
-
-(defmethod slot-unbound (class (obj bdd-typecase-node) (slot-name (eql 'dnf)))
-  (setf (slot-value obj 'dnf) (bdd-serialize obj)))
-
-(defmethod slot-unbound (class (obj bdd-typecase-node) (slot-name (eql 'expr)))
-  (setf (slot-value obj 'expr) (bdd-serialize obj)))
-
-(defmethod bdd-serialize ((leaf bdd-typecase-node))
-  `(satisfies ,(slot-value leaf 'satisfies)))
-
-(defmethod print-object ((bdd bdd-typecase-node) stream)
-  (print-unreadable-object (bdd stream :type t :identity nil)
-    (when (slot-boundp bdd 'ident)
-      (format stream "[~D]" (slot-value bdd 'ident)))
-    (format stream "~S" (bdd-label bdd))))
-
-(defmethod bdd-and ((bdd1 bdd-node) (leaf bdd-typecase-node))
-  (bdd-and bdd1 (bdd-proxy leaf)))
-
-(defmethod bdd-and ((leaf bdd-typecase-node) (bdd1 bdd-node))
-  (bdd-and (bdd-proxy leaf) bdd1))
-
-(defmethod bdd-and ((leaf1 bdd-typecase-node) (leaf2 bdd-typecase-node))
-  (bdd-and (bdd-proxy leaf1) (bdd-proxy leaf2)))
-
-
-(defmethod bdd-or ((bdd1 bdd-node) (leaf bdd-typecase-node))
-  (bdd-or bdd1 (bdd-proxy leaf)))
-
-(defmethod bdd-or ((leaf bdd-typecase-node) (bdd1 bdd-node))
-  (bdd-or (bdd-proxy leaf) bdd1))
-
-(defmethod bdd-or ((leaf1 bdd-typecase-node) (leaf2 bdd-typecase-node))
-  (bdd-or (bdd-proxy leaf1) (bdd-proxy leaf2)))
-
-
-(defmethod bdd-and-not ((bdd1 bdd-node) (leaf bdd-typecase-node))
-  (bdd-and-not bdd1 (bdd-proxy leaf)))
-
-(defmethod bdd-and-not ((leaf bdd-typecase-node) (bdd1 bdd-node))
-  (bdd-and-not (bdd-proxy leaf) bdd1))
-
-(defmethod bdd-and-not ((leaf1 bdd-typecase-node) (leaf2 bdd-typecase-node))
-  (bdd-and-not (bdd-proxy leaf1) (bdd-proxy leaf2)))
-
-(defmethod bdd-cmp-bdd ((bdd1 bdd-typecase-node) (bdd2 bdd-node))
-  '>)
-
-(defmethod bdd-cmp-bdd ((bdd1 bdd-node) (bdd2 bdd-typecase-node))
-  '<)
-
-(defmethod bdd-cmp-bdd ((bdd1 bdd-typecase-node) (bdd2 bdd-typecase-node))
-  (bdd-cmp (bdd-label bdd1) (bdd-label bdd2)))
-
-(defmethod bdd-find-reduction (label (bdd bdd-typecase-node) reduction-rules)
-  bdd)
-
-(defmethod bdd-reduce-allocated ((bdd bdd-typecase-node) new-left new-right)
-  bdd)
-
-(defmethod bdd-reduce-allocated ((bdd bdd-node) (new-left bdd-typecase-node) (new-right bdd-typecase-node))
-  bdd)
-
-(defmethod bdd-reduce-allocated ((bdd bdd-node) (new-left bdd-node) (new-right bdd-typecase-node))
-  (cond
-    ((bdd-type-equal bdd new-left)
-     new-left)
-    ((smarter-subtypep (bdd-to-expr bdd) nil)
-     *bdd-false*)
-    ((smarter-subtypep t (bdd-to-expr bdd))
-     *bdd-true*)
-    (t
-     bdd)))
-
-(defmethod bdd-reduce-allocated ((bdd bdd-node) (new-left bdd-typecase-node) (new-right bdd-node))
-  (cond
-    ((bdd-type-equal bdd new-right)
-     new-right)
-    ((smarter-subtypep (bdd-to-expr bdd) nil)
-     *bdd-false*)
-    ((smarter-subtypep t (bdd-to-expr bdd))
-     *bdd-true*)
-    (t
-     bdd)))
-
-
-(defmethod bdd-list-to-bdd ((head (eql :typecase-form)) tail)
-  (destructuring-bind (&key typecase-form satisfies) (cons head tail)
-    (make-instance 'bdd-typecase-node
-                   :satisfies satisfies 
-                   :label typecase-form)))
 
 (defvar *satisfies-symbols* t
   "The initial value is t so there will be an error if a function tries to push onto it without rebinding with dynamic extent.")
+
 (defun build-bdd-arg-from-typecase-body (type-case-body)
   (apply #'values
          (reduce (lambda (acc clause)
@@ -133,8 +37,7 @@
                                                        (typep obj type)))
                          (list `(or ,acc-type
                                     (and (not (or ,@leading-types)) ,type
-                                         (:typecase-form ,clause-body
-                                          :satisfies ,f )))
+                                         (satisfies ,f)))
                                (cons type leading-types))))))
                  type-case-body :initial-value '(nil nil))))
 
@@ -172,12 +75,6 @@
       `(lambda (,obj)
          (labels ,(mapcar #'label-function (mapcar #'car bdd->name-mapping))
              ,(branch bdd))))))
-
-;; (bdd-to-if-then-else-3 (bdd '(or (and sequence (not array))
-;;                                      number
-;;                               (and (not sequence) array))) 'X)
-      
-      
 
 (defun bdd-to-if-then-else-tagbody/go (bdd obj)
   "expand into linear size code as TAGBODY/GO, whose runtime is logrithmic and code size is linear"
