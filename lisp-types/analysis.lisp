c602905ff22efbe4fea941b854222cd3c1d470a5
Jim Newton <jnewton@lrde.epita.fr>
2018-03-19 15:55:40 +0100

    reduced number of tries of profiling

--- lisp-types/analysis.lisp
+++ lisp-types/analysis.lisp
@@ -1596,14 +1596,14 @@ sleeping before the code finishes evaluating."
 
 
 (defun bdd-report-profile (&key (re-run t) (multiplier 0.2) (destination-dir *destination-dir*)
-                             (prefix "bdd-profile-1-") (decomposition-functions *decomposition-functions*))
+                             (num-tries 4) (prefix "bdd-profile-1-") (decomposition-functions *decomposition-functions*))
   (big-test-report :re-run re-run
                    :profile t
                    :prefix prefix
                    :multiplier multiplier
                    :normalize nil
                    :time-out 200
-                   :num-tries 4
+                   :num-tries num-tries
                    :hilite-min nil
                    :destination-dir destination-dir
                    :decomposition-functions decomposition-functions))
